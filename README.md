f84f3aa42d188992867a1256f9f3015dec5e2845
v.moriceau <v.moriceau@cube-creative.com>
2019-08-22 15:45:46 +0200

    initial commit : setup, readme, etc

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# guibedos
+# GUI Bedos
 
-Les GUIs
+PySide widgets and helpers
+
+## Notes
+
+- CSS stylesheets and icons are borrowed from FreeCAD, more info 
+[here](https://github.com/FreeCAD/FreeCAD/tree/master/src/Gui/Stylesheets)
---
+++ setup.py
@@ -0,0 +1,42 @@
+from os import path
+from codecs import open
+from setuptools import setup, find_packages
+
+
+NAME = 'guibedos'
+VERSION = '0.0.0'
+DESCRIPTION = 'PySide widgets and helpers'
+AUTHOR = 'Cube Creative'
+AUTHOR_EMAIL = 'development@cube-creative.com'
+
+
+_here = path.abspath(path.dirname(__file__))
+_readme_filepath = path.join(_here, 'README.md')
+_requirements_filepath = path.join(_here, 'requirements.txt')
+
+
+if path.isfile(_readme_filepath):
+    with open(_readme_filepath, encoding='utf-8') as readme_file:
+        _long_description = readme_file.read()
+else:
+    _long_description = 'Unable to load README.md'
+
+
+if path.isfile(_requirements_filepath):
+    with open(_requirements_filepath) as requirements_file:
+        _requirements = requirements_file.readlines()
+else:
+    _requirements = list()
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=_long_description,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    packages=find_packages(exclude=['tests']),
+    install_requires=_requirements,
+    include_package_data=True
+)
