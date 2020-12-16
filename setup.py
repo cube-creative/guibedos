d06d377b867b7f5c65552b011a9a6bcd5ef77d4f
aMondelin <anthonydu77120@hotmail.fr>
2020-12-16 15:03:19 +0100

    fix: correct bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 NAME = 'guibedos'
-VERSION = '0.14.2'
+VERSION = '1.0.0'
 DESCRIPTION = 'PySide widgets and helpers'
 AUTHOR = 'Cube Creative'
 AUTHOR_EMAIL = 'development@cube-creative.com'
